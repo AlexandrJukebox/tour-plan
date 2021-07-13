c7067343dae8383a6f0a84ef13d965124b5d8220
AlexandrJukebox <321gizatulin@gmail.com>
2021-07-13 20:17:17 +0300

    create: added slider ArRight / ArLeft, effect

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  loop: true,
-
+  // Optional parameters 
+    loop: true,
+  // Transition effect ('slide' | 'fade' | 'cube' | 'coverflow' | 'flip')
+    effect: 'coverflow',
   // Navigation arrows
   navigation: {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
+// keyboard left+right+pageUp+pageDown
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+        pageUpDown: true,
+  },
+    
+// Speed	
   speed: 1000,
 });
