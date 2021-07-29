77df643f01dbaad3fa175dfea1b113bf9edb40d4
AlexandrJukebox <321gizatulin@gmail.com>
2021-07-29 15:57:33 +0300

    create: modal window

--- js/main.js
+++ js/main.js
@@ -1,51 +1,74 @@
-const hotelSwiper = new Swiper('.hotel-slider', {
-  // Optional parameters 
+$(document).ready(function () {
+  // Swiper 1 
+  const hotelSwiper = new Swiper('.hotel-slider', {
+    // Optional parameters 
     loop: true,
-  // Transition effect ('slide' | 'fade' | 'cube' | 'coverflow' | 'flip')
+    // Transition effect ('slide' | 'fade' | 'cube' | 'coverflow' | 'flip')
     effect: 'coverflow',
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-// keyboard left+right+pageUp+pageDown
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    // keyboard left+right+pageUp+pageDown
     keyboard: {
-        enabled: true,
-        onlyInViewport: false,
-        pageUpDown: true,
-  },
-    
-// Speed	
-  speed: 1000,
-});
-const reviewsSLider = new Swiper('.reviews-slider', {
-  // Optional parameters 
-    loop: true,
-  // Transition effect ('slide' | 'fade' | 'cube' | 'coverflow' | 'flip')
-    effect: 'slide',
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-// keyboard left+right+pageUp+pageDown
-    keyboard: {
-        enabled: true,
-        onlyInViewport: false,
-        pageUpDown: true,
-  },
-    
-// Speed	
-  speed: 500,
-});
-$('.parallax-window').parallax({
-  imageSrc: '/path/to/image.jpg',
-  speed: 0.2, 
-  parallax: 'scroll',
-  imageSrc:'img/newsletter-bg.jpg',
-});
+      enabled: true,
+      onlyInViewport: false,
+      pageUpDown: true,
+    },
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function() {
-  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible');
-})
+    // Speed	
+    speed: 1000,
+  });
+  // Swiper 2 
+  const reviewsSLider = new Swiper('.reviews-slider', {
+    // Optional parameters 
+    loop: true,
+    // Transition effect ('slide' | 'fade' | 'cube' | 'coverflow' | 'flip')
+    effect: 'slide',
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+    // keyboard left+right+pageUp+pageDown
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+      pageUpDown: true,
+    },
+
+    // Speed	
+    speed: 500,
+  });
+  //Паралакс эфект
+  $('.parallax-window').parallax({
+    imageSrc: 'img/newsletter-bg.jpg',
+    speed: 0.2,
+    parallax: 'scroll',
+  });
+  //Всплывающее меню
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function () {
+    $(".navbar-bottom").toggleClass('navbar-bottom--visible');
+  });
+  //Модальные окна
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    console.log($(this).attr("data-href"));
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+});
